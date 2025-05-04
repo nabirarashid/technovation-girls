ad94dbc314dfc4ba47c238c279b9fcfc9762fab4
Nabira Rashid <163450706+nabirarashid@users.noreply.github.com>
2025-05-04 14:17:52 -0400

    Create create_database.py

---
+++ create_database.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+def make_database():
+    connect = sqlite3.connect("businesses.db")
+    cursor = connect.cursor()
+
+    # Create businesses table (with LATITUDE and LONGITUDE)
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS BUSINESSES (
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        NAME TEXT NOT NULL,
+        LATITUDE REAL,
+        LONGITUDE REAL
+    );
+    ''')
+
+    # Create products table
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS PRODUCTS(
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        BUSINESS_ID INTEGER NOT NULL,
+        PRODUCT_NAME TEXT NOT NULL,
+        PRODUCT_PRICE REAL NOT NULL,
+        PRODUCT_DESCRIPTION TEXT NOT NULL,
+        PRODUCT_TAGS TEXT,
+        PRODUCT_IMAGE BLOB NOT NULL,
+        FOREIGN KEY (BUSINESS_ID) REFERENCES BUSINESSES(ID)
+    );
+    ''')
+
+    connect.commit()
+    connect.close()
